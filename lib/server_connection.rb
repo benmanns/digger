174232b09cf09a13f83b538752645aad89faadb7
Benjamin Manns <benmanns@gmail.com>
2010-11-24 21:08:16 -0500

    Set up server to split up packets.

--- lib/server_connection.rb
+++ lib/server_connection.rb
@@ -6,8 +6,23 @@ module Digger
       @client_connection = client_connection
     end
 
+    def post_init
+      @buffer = ''
+    end
+
     def receive_data(data)
-      @client_connection.send_data(data)
+      @buffer << data
+      unless @buffer.length == 0
+        packet_class = Digger::Packets::Packet.get_class(@buffer, :mode => :server)
+        unless packet_class == Digger::Packets::Packet
+          length = packet_class.get_length(@buffer, :mode => :server)
+          if length and @buffer.length >= length
+            @client_connection.send_data(@buffer.slice!(0, length))
+          end
+        else
+          raise "Unknown packet id 0x#{@buffer.unpack('C').first.to_s(16)}: #{@buffer.dump}"
+        end
+      end
     end
 
     def unbind
